c9eaadc6f25662fe06a00e4e464810fa4106a044
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-05-29 12:53:46 +0200

    Creazione modulo uploads con gestione errori

---
+++ src/backend/routes/upload.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('middleware/uploadMiddleware')
+
+server.post('upload', upload.array('photos', 10), (req, res) => {
+    if (!req.files) {
+        return res.status(400).json({ message: "Nessuna immagine caricata, riprovare per favore!" })
+    }
+
+    const filePaths = req.file.map(file => "/   uploads/pending/${file.filename}")
+
+    res.status(200).json( 
+        {
+            message: 'File caricati con successo!',
+            files: filePaths
+        }
+    );
+});
+
+modules.export = router;
