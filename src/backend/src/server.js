57d5240f0e2e59787673c493457a0d25ca4daded
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-05-29 14:01:14 +0200

    Aggiunta gestione visualizzazione intera cartella file

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const uploadRoutes = require('../routes/upload');
+const serveIndex = require('serve-index');
 
 const server = express();
 const PORT = 3001;
@@ -11,10 +13,15 @@ server.get('/', (_req, res) => {
     res.send("Hello World");
 })
 
+const path = require('path');
+
+// server.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+server.use('/api', uploadRoutes);
+server.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')), serveIndex(path.join(__dirname, '..', 'uploads'), {'icons': true})); // gestione intera cartella file
+
 server.use((req, res) => {
   res.status(404).send('Pagina non Trovata');
 });
-
 
 server.listen(PORT, () => {
     console.log('Server in Ascolto!')
